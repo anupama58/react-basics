61f4e5aa1b2fb4f6504102638c2e2360a73f6d7b
Anupama Pawar <anupama.pawar@sts.local>
2021-08-16 15:41:30 +0530

    understanding of basic class components and passing data using props

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import TodoContainer from "./TodoContainer"
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <TodoContainer />, 
   </React.StrictMode>,
+ 
   document.getElementById('root')
+
 );
 
 // If you want to start measuring performance in your app, pass a function
