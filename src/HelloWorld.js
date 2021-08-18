d3f1ee99785fb03c0c51dc16c86f52716b376357
Anupama Pawar <anupama.pawar@sts.local>
2021-08-18 21:09:08 +0530

    functional components and addition of some hooks

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const HelloWorld = () =>{
 
+    const [counter, setCounter] = useState(0);
+    const [currentInput,setInput] = useState('Tell me your name');
+    
+    useEffect(()=>{
+        console.log(`hello ${currentInput}`);
+    },[currentInput])
+    
     function sayHello(){
-        alert('Hello World!')
+        alert('Hello World');        
+        setCounter(counter+1);
     };
 
+ 
+    const changeInput=(event)=>{
+        const newInput = event.target.value;
+        const currentInput =newInput
+        setInput(currentInput);
+
+    }
     return (
-        <button onClick={sayHello}>Click Me!</button>
+        <div>
+            <button onClick={sayHello}>Click Me!</button>
+            <p>How many times i clicked {counter}</p>     
+            <input placeholder='Enter here'  onChange={changeInput}></input> {currentInput}
+        </div>
     );
 };
 
